83de57dd4dbe1c322ff47079bc437b64fb330c98
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2017-06-25 19:33:51 +0100

    All arrow heads seem to be ok now

--- client/DrawingAreaSvg.js
+++ client/DrawingAreaSvg.js
@@ -231,29 +231,30 @@ export class DrawingAreaSvg {
     drawNormalRightLink(n1, n2) {
         if (n2.colIndex > n1.colIndex) {
             this.createLine(n1.nex, n1.ney, n2.x, n1.ney, 'green', null, true);
-            this.drawArrowHeadRight({ x: n1.nex + (n1.width / 2) + 1, y: n1.ney }, 'green');
-            this.drawArrowHeadRight({ x: n2.x - 1, y: n1.ney }, 'green');
         }
         else {
             this.createLine(n1.nex, n1.ney, this.width, n1.ney, 'green', null, true);
             this.createLine(0, n2.nwy, n2.x, n2.nwy, 'green', null, true);
         }
+        this.drawArrowHeadRight({ x: n1.nex + (n1.width / 2) + 1, y: n1.ney }, 'green');
+        this.drawArrowHeadRight({ x: n2.x - 1, y: n1.ney }, 'green');
     }
 
     drawGoingAroundRightLink(n1, n2, tweeners) {
         this.drawTopGoingAroundLink(n1, n2, tweeners);
+        this.drawArrowHeadRight({ x: n1.nex + (n1.width / 2) + 1, y: n1.ney }, 'red');
     }
 
     drawNormalLeftLink(n1, n2) {
         if (n1.colIndex > n2.colIndex) {
             this.createLine(n1.swx, n1.swy, n2.x + n2.width, n1.swy, 'green', null, true);
-            this.drawArrowHeadLeft({ x: n1.swx - (n1.width / 2) - 1, y: n1.swy }, 'green');
-            this.drawArrowHeadLeft({ x: n2.x + n2.width + 1, y: n1.swy }, 'green');
         }
         else {
             this.createLine(n1.swx, n1.swy, 0, n1.swy, 'green', null, true);
             this.createLine(this.width, n2.sey, n2.x + n2.width, n2.sey, 'green', null, true);
         }
+        this.drawArrowHeadLeft({ x: n1.swx - (n1.width / 2) - 1, y: n1.swy }, 'green');
+        this.drawArrowHeadLeft({ x: n2.x + n2.width + 1, y: n1.swy }, 'green');
     }
 
     drawGoingAroundLeftLink(n1, n2, tweeners) {
@@ -264,34 +265,36 @@ export class DrawingAreaSvg {
             this.createLine(n1.swx, n1.swy, 0, n1.swy, 'red', null, true);
             this.drawBottomGoingRoundLink(n1, n2, tweeners, true);
         }
+        this.drawArrowHeadLeft({ x: n1.swx - (n1.width / 2) - 1, y: n1.swy }, 'red');
     }
 
     drawNormalDownLink(n1, n2) {
         if (n2.rowIndex > n1.rowIndex) {
             this.createLine(n1.sex, n1.sey, n1.sex, n2.y, 'green', null, true);
-            this.drawArrowHeadDown({ x: n1.sex, y: n1.sey + (n1.width / 2) + 1 }, 'green');
-            this.drawArrowHeadDown({ x: n1.nex, y: n2.y - 1 }, 'green');
         }
         else {
             this.createLine(n1.sex, n1.sey, n1.sex, this.height, 'green', null, true);
             this.createLine(n2.nex, 0, n2.nex, n2.y, 'green', null, true);
         }
+        this.drawArrowHeadDown({ x: n1.sex, y: n1.sey + (n1.width / 2) + 1 }, 'green');
+        this.drawArrowHeadDown({ x: n1.nex, y: n2.y - 1 }, 'green');
     }
 
     drawGoingAroundDownLink(n1, n2, tweeners) {
         this.drawRightGoingAroundLink(n1, n2, tweeners);
+        this.drawArrowHeadDown({ x: n1.sex, y: n1.sey + (n1.width / 2) + 1 }, 'red');
     }
 
     drawNormalUpLink(n1, n2) {
         if (n1.rowIndex > n2.rowIndex) {
             this.createLine(n1.nwx, n1.nwy, n1.nwx, n2.y + n2.height, 'green', null, true);
-            this.drawArrowHeadUp({ x: n1.nwx, y: n1.ney - (n1.width / 2) - 1 }, 'green');
-            this.drawArrowHeadUp({ x: n2.swx, y: n2.y + n2.height + 1 }, 'green');
         }
         else {
             this.createLine(n2.swx, this.height, n2.swx, n2.y + n2.height, 'green', null, true);
             this.createLine(n1.nwx, n1.nwy, n1.nwx, 0, 'green', null, true);
         }
+        this.drawArrowHeadUp({ x: n1.nwx, y: n1.ney - (n1.width / 2) - 1 }, 'green');
+        this.drawArrowHeadUp({ x: n2.swx, y: n2.y + n2.height + 1 }, 'green');
     }
 
     drawGoingAroundUpLink(n1, n2, tweeners) {
@@ -302,6 +305,7 @@ export class DrawingAreaSvg {
             this.createLine(n1.nwx, n1.nwy, n1.nwx, 0, 'red', null, true);
             this.drawLeftGoingRoundLink(n1, n2, tweeners, true);
         }
+        this.drawArrowHeadUp({ x: n1.nwx, y: n1.ney - (n1.width / 2) - 1 }, 'red');
     }
 
     removeLinks() {
